1ab0092f30997c137445a34956c437d0357658fb
JC Hipolito <jchips12@gmail.com>
2017-03-02 17:41:41 +0800

    rename to nba-dtf-ui

--- nba-dtf/app/login/login.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'login-app',
-    templateUrl: './app/login/login.component.html',
-    styleUrls: ['./app/login/login.component.css']
-})
-export class LoginComponent {}
--- nba-dtf/app/sign-up/sign-up.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'sign-up-app',
-    templateUrl: './app/sign-up/sign-up.component.html',
-    styleUrls: ['./app/sign-up/sign-up.component.css']
-})
-export class SignUpComponent { }
